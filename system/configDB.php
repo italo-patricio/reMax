6fb17d4f522f2a1c8eec78c970bf81c1642b5d70
Italo <italopithu@hotmail.com>
2014-01-14 14:14:06 -0200

    #1 update

--- system/configDB.php
+++ system/configDB.php
@@ -7,10 +7,10 @@
  */
   class configDB{
 
-            protected $db_host  = 'localhost';
+            protected $db_host  = 'mysql.hostinger.com.br';
             protected $db_driver = 'mysql';
             protected $db_port ='3306';
-            protected $db_user = 'root';
-            protected $db_senha = '';
-            protected $db_name = 'reabilitacaomaxilar';
+            protected $db_user = 'u480013025_root';
+            protected $db_senha = '123456';
+            protected $db_name = 'u480013025_rbm';
    }
